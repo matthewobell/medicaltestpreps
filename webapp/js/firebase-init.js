7a2ef9b52944117e34dadc2afda4de20fa786721
matthewobell <matt.o.bell@gmail.com>
2026-03-10 12:26:11 -0700

    Update firebase-init.js

--- webapp/js/firebase-init.js
+++ webapp/js/firebase-init.js
@@ -3,21 +3,20 @@
 // -------------------------------------
 
 // Import Firebase libraries
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+import { getFirestore, serverTimestamp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 
-// Your Firebase configuration
+// Firebase configuration
 const firebaseConfig = {
-
   apiKey: "AIzaSyBWe_6sDJOTmsNoRbuPnVHISA8jlwnDab8",
   authDomain: "medical-test-prep.firebaseapp.com",
   projectId: "medical-test-prep",
-  storageBucket: "medical-test-prep.appspot.com",
+  storageBucket: "medical-test-prep.firebasestorage.app",
   messagingSenderId: "106764694288",
-  appId: "1:106764694288:web:b0aad15921ee12d956b95"
-
+  appId: "1:106764694288:web:b0aa0d15921ee12d956b95",
+  measurementId: "G-F46FLL5GZH"
 };
 
 
@@ -30,6 +29,7 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 
-// Make available globally
+// Make services available globally
 window.firebaseAuth = auth;
 window.firebaseDB = db;
+window.serverTimestamp = serverTimestamp;
